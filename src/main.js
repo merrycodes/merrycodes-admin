65c2ef3db67386ab1abb20ff41e596c3d8eab1da
鲁涛 <merrycodes@163.com>
2020-04-17 22:29:32 +0800

    :tada:优化项目（未完成）

--- src/main.js
+++ src/main.js
@@ -1,33 +1,20 @@
 import Vue from 'vue'
-
 import Cookies from 'js-cookie'
-
 import Element from 'element-ui'
 import './styles/element-variables.scss'
-
-import '@/styles/index.scss' // global css
-
+import '@/styles/index.scss'
 import App from './App'
 import store from './store'
 import router from './router'
-
-import './icons' // icon
-import './permission' // permission control
-import './utils/error-log' // error log
+import './icons'
+import './permission'
+import './utils/error-log'
 import { FUNCTIONS } from './utils/merry-codes'
-
-import * as filters from './filters' // global filters
 
 Vue.prototype.$util = FUNCTIONS
 
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium' // set element-ui default size
-})
-
-// register global utility filters
-Object.keys(filters).forEach(key => {
-  console.log(key, filters[key])
-  Vue.filter(key, filters[key])
+  size: Cookies.get('size') || 'medium'
 })
 
 Vue.config.productionTip = false
